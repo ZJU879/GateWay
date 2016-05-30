52dd5208b10e300dbb7ebe36ceadc78e800d879c
JeremyJiWZ <mr.ji@icloud.com>
2016-05-30 20:15:33 +0800

    modify plc module

--- testPLC.c
+++ testPLC.c
@@ -1,19 +1,13 @@
-#include "serial.h"
+#include "plc.h"
 
 int main(){
 	int n=0;
 	int len=0;
+	printf("hello\n");
 	PLC_init();
-	sleep(2);
+	// sleep(2);
 	get4plc();
-	printf("%s\n", plc_read_buf);
 
-	plc_write_buf[0]='h';
-	plc_write_buf[0]='e';
-	plc_write_buf[0]='l';
-	plc_write_buf[0]='l';
-	plc_write_buf[0]='o';
-	plc_write_buf[0]='\n';
-	send2plc();
+	return 0;
 
 }
